ccfb4c7a45ce6689cddb6e136ee26ffde1afa398
hustin-paul <hj.paul7@gmail.com>
2021-12-18 10:22:31 -0500

    updated scripts

--- .local/bin/scripts/polybar_net_indicator.sh
+++ .local/bin/scripts/polybar_net_indicator.sh
@@ -10,33 +10,35 @@ source /home/hustin/.local/bin/scripts/colors.sh
 
 net_print() {
 
-	CONNECTED_WIFI=$(iwconfig wlp4s0 | grep ESSID | wc -l)
+	CONNECTED_WIFI=$(cat /sys/class/net/wlp4s0/carrier)
 	#ESSID=$(iwconfig wlan0 | grep ESSID | cut -d: -f2 | xargs)
-	[ "$ESSID" = "off/any" ] && CONNECTED_WIFI=0 || CONNECTED_WIFI=1
-	CONNECTED_VPN=$(ifconfig -a | grep tun0 | wc -l)
+	#[ "$ESSID" = "off/any" ] && CONNECTED_WIFI=0 || CONNECTED_WIFI=1
+	# CONNECTED_VPN=$(ifconfig -a | grep tun0 | wc -l)
+        CONNECTED_VPN=$(printf "VPN: " && (pgrep -a openvpn$ | head -n 1 | awk '{print $NF }' | cut -d '.' -f 1 && echo down) | head -n 1)
+        CONNECTED_LAN=$(cat /sys/class/net/enp3s0/carrier)
 	
-	if [ "$CONNECTED_WIFI" -eq 1 ]; then
+	if [ "$CONNECTED_WIFI" = 1 ]; then
 		wifi_indicator="${bright_blue}直${RESET}"
 	else
 		wifi_indicator="${dark0_soft}直${RESET}"
 	fi
 
-	if [ "$CONNECTED_VPN" -eq 1 ]; then
-		vpn_indicator="${bright_blue}${RESET}"
-	else
-		vpn_indicator="${dark0_soft}${RESET}"
-	fi
+        if [ "$CONNECTED_LAN" -eq 1 ]; then
+                lan_indicator="${bright_blue}ﯱ ${RESET}"
+        else
+                lan_indicator="${dark0_soft}ﯱ ${RESET}"
+        fi
 
-	# if [ "$CONNECTED_HAMACHI" = "logged in" ]; then
-	# 	hamachi_indicator="${bright_green}${RESET}"
-	# else
-	# 	hamachi_indicator="${dark0_soft}${RESET}"
-	# fi
+	if [ "$CONNECTED_VPN" = "VPN: nm-openvpn" ]; then
+		vpn_indicator="${bright_blue} ${RESET}"
+	else
+		vpn_indicator="${dark0_soft} ${RESET}"
+	fi
 
 	
 	
 	# echo "$dark2[$wifi_indicator$dark2]$dark2[$vpn_indicator$dark2]"
-	echo "$dark0_soft[ $wifi_indicator $vpn_indicator$dark0_soft]"
+	echo "$dark0_soft[ $wifi_indicator  $lan_indicator $vpn_indicator$dark0_soft]"
 	# echo " $wifi_indicator $vpn_indicator "
 }
 
